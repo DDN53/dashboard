d95bf082634ff13f47402d925c25c8778363d671
DDN53 <dinukanisheda9@gmail.com>
2025-01-15 21:21:56 +0530

    finished 9

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,22 +1,19 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
 import { fetchRevenue } from '@/app/lib/data';
+
 
 export default async function RevenueChart() {
   const revenue = await fetchRevenue(); // Fetch revenue data
 
   const chartHeight = 350;
-
-  // Ensure revenue is valid
+  
+  // Generate Y-axis labels and top label
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
-
-  // Generate Y-axis labels and top label
-  const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
